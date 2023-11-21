24e55ba4887eb513aee6a11eacd19ecdf6042b11
Ike <malachite.wind@gmail.com>
2023-11-21 13:25:43 -0800

    update: formatting

--- simple_example.py
+++ simple_example.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def run(seed, amplitude):
-    """Deterimne if the maximum value of the sine function equals ``amplitude``"""
+    """
+    Deterimne if the maximum value of the sine function equals ``amplitude``
+    """
     x = np.arange(0, 10, .05)
     y = amplitude * np.sin(x)
     err = np.abs(max(y) - amplitude)
