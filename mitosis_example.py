b9afc6c4fea588d76001346d396eacad4d996a93
Ike <malachite.wind@gmail.com>
2023-11-29 15:43:35 +0000

    update

--- mitosis_example.py
+++ mitosis_example.py
@@ -5,6 +5,6 @@ from pathlib import Path
 folder = Path(".").resolve()
 params = [mitosis.Parameter("my_variant", "amplitude", 4)]
 
-# mitosis.run(simple_example, params=params, trials_folder=folder)
+mitosis.run(simple_example, params=params, trials_folder=folder)
 
 # print(params[0].id_name)
