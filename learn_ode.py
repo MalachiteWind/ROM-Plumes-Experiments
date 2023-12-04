a9dc96379cca50b3eb1c9a57f7fff957b29222cd
Ike <malachite.wind@gmail.com>
2023-12-04 10:26:02 +0000

    to run bad-seed exp

--- learn_ode.py
+++ learn_ode.py
@@ -1,13 +1,17 @@
 import sindy_pipeline 
 import pickle
 import numpy as np
+import mitosis
+from pathlib import Path
+
+
+folder = Path(".").resolve()
 
 # Load data 
 pickle_path = "plume_videos/July_20/video_low_1/gauss_blur_coeff.pkl"
 
 with open(pickle_path, 'rb') as f:
     loaded_arrays = pickle.load(f)
-
 
 # Params
 time_series = loaded_arrays["mean"]
@@ -17,23 +21,39 @@ ensem_alpha=1e-3
 ensem_max_iter=100
 poly_degree=3
 ensem_num_models=40
-# ensem_time_points = 100
-ensem_time_points = None
+ensem_time_points = None # ensem_time_points = 100
+seed =1234
 
-seed =123
+params = [
+    mitosis.Parameter("bad_seed", "seed", seed),
+    mitosis.Parameter("july_20_low_1", "time_series", time_series),
+    mitosis.Parameter("window_length","window_length",window_length),
+    mitosis.Parameter("ensem_thresh","ensem_thresh", ensem_thresh), 
+    mitosis.Parameter("ensem_alpha", "ensem_alpha", ensem_alpha),
+    mitosis.Parameter("ensem_max_iter", "ensem_max_iter", ensem_max_iter),
+    mitosis.Parameter("poly_degree", "poly_degree", 3),
+    mitosis.Parameter("ensem_num_models", "ensem_num_model", 40),
+    mitosis.Parameter("ensem_time_points", "ensem_time_poitns", ensem_time_points)
+]
 
 # Run experiment
-sindy_pipeline.run(
-    time_series=time_series,
-    window_length=window_length,
-    ensem_thresh=ensem_thresh,
-    ensem_alpha=ensem_alpha,
-    ensem_max_iter=ensem_max_iter,
-    ensem_num_models=ensem_num_models,
-    ensem_time_points=ensem_time_points,
-    poly_degree=poly_degree,
-    seed=seed
+mitosis.run(
+    sindy_pipeline,
+    params=params,
+    debug=True,
+    trials_folder=folder
 )
+# sindy_pipeline.run(
+#     time_series=time_series,
+#     window_length=window_length,
+#     ensem_thresh=ensem_thresh,
+#     ensem_alpha=ensem_alpha,
+#     ensem_max_iter=ensem_max_iter,
+#     ensem_num_models=ensem_num_models,
+#     ensem_time_points=ensem_time_points,
+#     poly_degree=poly_degree,
+#     seed=seed
+# )
 
 # Good seeds
 # -12
