621479e470966ef7f6dec98d64fdfe1cdb275cde
Ike <malachite.wind@gmail.com>
2024-05-31 16:02:39 -0700

    added helper functions and test code

--- plumex/regression_pipeline.py
+++ plumex/regression_pipeline.py
@@ -5,6 +5,9 @@ import numpy as np
 
 from typing import List
 from typing import Any
+from typing import Optional
+from typing import Callable
+from typing import Literal
 from .types import Frame
 from .types import PlumePoints
 from .types import NpFlt
@@ -53,9 +56,104 @@ def split_into_train_val(
     return train_set, val_set
 
 
-def get_L2_acc(X_true:np.ndarray[Any,NpFlt], X_pred:np.ndarray[Any,NpFlt]) -> float:
+def _get_L2_acc(X_true:np.ndarray[Any,NpFlt], X_pred:np.ndarray[Any,NpFlt]) -> float:
     """
     Get L2 accuracy between two arrays
     """
     err = np.linalg.norm(X_true-X_pred)/np.linalg.norm(X_true)
     return 1-err
+
+def _construct_f(coef: Float1D, regression_method:Optional[str]=None) -> Callable[[float],float] | Callable[[float],Float1D]:
+    """construction function f based on coefficients and regression_method
+
+    Parameters:
+    ----------
+    coef:
+        array of poly coefficients in descending degree order.
+    
+    regression_method:
+        makes function parametric if regression_method = "poly_para". 
+    
+    Returns:
+    --------
+        f:
+            Callable function that takes float as argument.
+    """
+    if regression_method == "poly_para":
+        mid_index = len(coef)//2
+        f1 = np.polynomial.Polynomial(coef[:mid_index][::-1])
+        f2 = np.polynomial.Polynomial(coef[mid_index:][::-1])
+        def f(x):
+            return np.array([f1(x), f2(x)])
+    else: 
+        f = np.polynomial.Polynomial(coef[::-1])
+    return f
+
+def _get_true_pred(
+        func: Callable[[float],float] | Callable[[float],Float1D], 
+        r_x_y: PlumePoints, 
+        regression_method: str
+) -> tuple[Float1D,Float1D] | tuple[Float2D,Float2D]:
+    """
+    Acquire true coordinates from Frame points and predicted points from 
+    learned function based on regression_method used.
+    """
+    if regression_method == "poly" or regression_method == "linear":
+        y_pred = func(r_x_y[:,1])
+        y_true = r_x_y[:,2]
+
+    if regression_method == "poly_inv":
+        y_pred = func(r_x_y[:,2])
+        y_true = r_x_y[:,1]
+
+    if regression_method == "poly_para":
+        y_pred = func(r_x_y[:,0]).T
+        y_true = r_x_y[:,1:]
+    
+    return y_true, y_pred
+
+
+
+def get_coef_acc(
+        coef_time_series:np.ndarray[Any, NpFlt],
+        train_val_set: List[tuple[Frame,PlumePoints]], 
+        regression_method: str
+) -> Float1D:
+    """Get the L2 accuracy of learned coefficients on PlumePoints"""
+    if len(coef_time_series) != len(train_val_set):
+        raise TypeError(f"length of arrays must match")
+    
+    n_frames = len(coef_time_series)
+    accs = np.zeros(n_frames)
+    for i in range(n_frames):
+        coef_i = coef_time_series[i]
+
+        f = _construct_f(coef_i,regression_method)
+        _, r_x_y = train_val_set[i]
+
+        y_true, y_pred = _get_true_pred(f,r_x_y, regression_method)
+        accs[i] = _get_L2_acc(y_true,y_pred) 
+    
+    return accs
+
+
+def regress_video(
+        mean_points:  List[tuple[Frame, PlumePoints]],
+        x_split: int,
+        regression_method: str,
+        poly_deg: int = 2,
+        decenter: Optional[tuple[int,int]] = None
+):
+    """
+    """
+    train_set, val_set = split_into_train_val(mean_points, x_split)
+
+    coef_time_series = PLUME.regress_multiframe_mean(
+        mean_points=train_set,
+        regression_method=regression_method,
+        poly_deg=poly_deg,
+        decenter=decenter
+    )
+    train_acc = get_coef_acc(coef_time_series,train_set, regression_method)
+    val_acc = get_coef_acc(coef_time_series, val_set, regression_method)
+
--- plumex/tests/test_regression_pipeline.py
+++ plumex/tests/test_regression_pipeline.py
@@ -1,6 +1,31 @@
+from ..regression_pipeline import get_coef_acc
 from ..regression_pipeline import split_into_train_val
-from ..regression_pipeline import get_L2_acc
+from ..regression_pipeline import _get_L2_acc
+from ..regression_pipeline import _construct_f
+from ..regression_pipeline import _get_true_pred
+
 import numpy as np
+
+def test_get_coef_acc():
+    coef_time_series = np.array([
+        [1,2,3],
+        [3,2,1]
+    ])
+    R = np.array([0,1,2])
+    f1 = lambda x: x**2 + 2*x + 3
+    f2 = lambda x: 3*x**2 + 2*x + 1
+    r_x_y_1 = np.vstack((R,R,f1(R))).T
+    r_x_y_2 = np.vstack((R,R,f2(R))).T
+
+    train_val_set = [
+        (1,r_x_y_1),
+        (2,r_x_y_2)
+    ]
+
+    result = get_coef_acc(coef_time_series,train_val_set=train_val_set,regression_method='poly')
+    expected = np.array([1,1])
+
+    np.testing.assert_array_almost_equal(expected,result)
 
 def test_split_into_train_val():
     train_i = np.arange(6)+4
@@ -36,8 +61,89 @@ def test_get_L2_acc():
     x_pred = np.array([1,1])
 
     expected = 0
-    result = get_L2_acc(x_true, x_pred)
+    result = _get_L2_acc(x_true, x_pred)
     np.testing.assert_almost_equal(expected,result)
+
+def test_construct_f():
+    coef = (1,2,3)
+    f = _construct_f(coef)
+
+    expected = np.array([3,6,11])
+
+    result = f(np.array([0,1,2]))
+
+    np.testing.assert_array_almost_equal(expected,result)
+
+    # Test Parametric
+    coef = np.array([1,2,3,3,2,1])
+    f = _construct_f(coef,regression_method='poly_para')
+    expected = np.array([
+        [3,1],
+        [6,6],
+        [11,17]]
+    )
+    result = f(np.array([0,1,2])).T
+
+    np.testing.assert_array_almost_equal(expected,result)
+    
+
+def test_get_true_pred():
+    # Test "linear"
+    R = np.array([0,1,2])
+    f = lambda x: 2*x + 3
+    r_x_y = np.vstack((R,R,f(R))).T
+
+    result_true, result_pred = _get_true_pred(f,r_x_y,regression_method='linear')
+
+    expected = np.array([3,5,7])
+
+    np.testing.assert_array_almost_equal(expected,result_true)
+    np.testing.assert_array_almost_equal(expected,result_pred)
+
+    # Test "poly" and "linear"
+    R = np.array([0,1,2])
+    f = lambda x: x**2 + 2*x + 3
+    r_x_y = np.vstack((R,R,f(R))).T
+
+    result_true, result_pred = _get_true_pred(f,r_x_y,regression_method='poly')
+
+    expected = np.array([3,6,11])
+
+    np.testing.assert_array_almost_equal(expected,result_true)
+    np.testing.assert_array_almost_equal(expected,result_pred)
+
+    # Test "poly_inv"
+    R = np.array([0,1,2])
+    f = lambda x: x**2 + 2*x + 3
+    r_x_y = np.vstack((R,f(R),R)).T
+
+    result_true, result_pred = _get_true_pred(f,r_x_y,regression_method='poly_inv')
+
+    expected = np.array([3,6,11])
+
+    np.testing.assert_array_almost_equal(expected,result_true)
+    np.testing.assert_array_almost_equal(expected,result_pred)
+    
+    # Test "poly_para"
+    R = np.array([0,1,2])
+    f = _construct_f(np.array([1,2,3,3,2,1]),regression_method='poly_para')
+    r_x_y = np.vstack((R,f(R))).T
+
+    result_true, result_pred = _get_true_pred(f,r_x_y,regression_method='poly_para')
+
+    expected = np.array([
+        [3,1],
+        [6,6],
+        [11,17]]
+    )
+
+    np.testing.assert_array_almost_equal(expected,result_true)
+    np.testing.assert_array_almost_equal(expected,result_pred)
+
+
+
+
+
     
 
     
