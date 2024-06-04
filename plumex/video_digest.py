fbc4ae53129690522e2fcdeea334ea8a4bd8d8bf
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-06-03 18:48:50 -0700

    feat! (video_digest): Make use numpy arrays, not videos
    Adapt to current ara_plumes API as of commit b18dab2
    
    Also add visualization of fit

--- plumex/video_digest.py
+++ plumex/video_digest.py
@@ -1,20 +1,22 @@
 import pickle
-from typing import Any, Optional
+from typing import Any
 
 from ara_plumes import PLUME
+from ara_plumes.typing import GrayVideo, Frame, PlumePoints
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+
 from .data import pickle_path
 from .types import PolyData
 
 
 def create_centerline(
     filename: str,
-    img_range: Optional[list[int]] = None,
-    subtraction_method: str="fixed",
-    fixed_range: Optional[int]=None,
+    img_range: tuple[int]=(0, -1),
+    fixed_range: tuple[int]=(0, -1),
     gauss_kw: dict[str, Any]=None,
-    regression_method: str="sinusoid",
     circle_kw: dict[str, Any]=None,
-    regression_kw: dict[str, Any]=None,
+    contour_kws: dict[str, Any]=None,
 ) -> dict[str, PolyData]:
     """Calculate the centerline path from a moviefile
 
@@ -27,24 +29,48 @@ def create_centerline(
         gauss_kw = {}
     if circle_kw is None:
         circle_kw = {}
-    if regression_kw is None:
-        regression_kw = {}
+    if contour_kws is None:
+        contour_kws = {}
 
     origin_filename = filename + "_ctr.pkl"
     with open(pickle_path / origin_filename, "rb") as fh:
         origin = pickle.load(fh)
     plume = PLUME()
-    plume.read_video(str(pickle_path / filename))
+    np_filename = filename[:-3]+ "pkl" # replace mov with pkl
+    with open(pickle_path / np_filename, "rb") as fh:
+        plume.numpy_frames = pickle.load(fh)
     plume.orig_center = tuple(int(coord) for coord in origin)
-    center, _, _ = plume.train(
+    center, bottom, top = plume.train(
         img_range=img_range,
-        subtraction_method=subtraction_method,
         fixed_range=fixed_range,
-        display_vid=False,
-        regression_method=regression_method,
         concentric_circle_kws=circle_kw,
-        regression_kws=regression_kw,
+        get_contour_kws=contour_kws,
         **gauss_kw
     )
+    visualize_points(plume.numpy_frames)
     return {"main": None, "data": center}
 
+
+def visualize_points(
+    vid: GrayVideo,
+    center: list[tuple[Frame, PlumePoints]],
+    bottom: list[tuple[Frame, PlumePoints]],
+    top: list[tuple[Frame, PlumePoints]],
+    n_plots: int
+) -> Figure:
+    n_plots = 9
+    min_frame_t = center[0][0]
+    max_frame_t = center[-1][0]
+    plot_frameskip = (max_frame_t - min_frame_t) / n_plots
+    frame_ids = [int(plot_frameskip * i) for i in range(n_plots)]
+    fig, axes = plt.subplots((n_plots + 2) // 3, 3)
+    for frame_id, ax in zip(frame_ids, axes.flatten()):
+        frame_t, frame_center = center[frame_id]
+        _, frame_bottom = bottom[frame_id]
+        _, frame_top = bottom[frame_id]
+        ax.imshow(vid[frame_t])
+        ax.plot(frame_center[:, 1], frame_center[:, 2], "r.")
+        ax.plot(frame_bottom[:, 1], frame_bottom[:, 2], "b.")
+        ax.plot(frame_top[:, 1], frame_top[:, 2], "b.")
+        ax.set_title(f"Frame {frame_t}")
+    return fig
