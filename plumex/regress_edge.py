02c37e27ba09e50c1bb0bc67f91fbcf57076f873
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-07-31 15:23:18 -0700

    WIP: create train/test sets

--- plumex/regress_edge.py
+++ plumex/regress_edge.py
@@ -7,7 +7,12 @@ import numpy as np
 
 
 # run after video_digest
-def regress_edge(data:dict):
+def regress_edge(data:dict,
+                 training: float,
+                 randomize: bool = True,
+                 seed: int = 1234
+):
+    np.random.seed(seed=seed)
     center = cast(List[tuple[int,PlumePoints]],data["center"])
     bot = cast(List[tuple[int,PlumePoints]],data["bottom"])
     top = cast(List[tuple[int,PlumePoints]],data["top"])
@@ -22,11 +27,37 @@ def regress_edge(data:dict):
         rad_dist_bot = flatten_edge_points(center_pp,bot_pp)
         rad_dist_top = flatten_edge_points(center_pp,top_pp)
 
-        t_rad_dist_bot = np.hstack((t*np.ones(len(rad_dist_bot).reshape(-1,1),rad_dist_bot)))
-        t_rad_dist_top = np.hstack((t*np.ones(len(rad_dist_top).reshape(-1,1),rad_dist_top)))
+        t_rad_dist_bot = np.hstack((t*np.ones(len(rad_dist_bot),1),rad_dist_bot))
+        t_rad_dist_top = np.hstack((t*np.ones(len(rad_dist_top),1),rad_dist_top))
 
         bot_flattened.append(t_rad_dist_bot)
         top_flattened.append(t_rad_dist_top)
+    
+    top_flattened = np.concatenate(top_flattened,axis=0)
+    bot_flattened = np.concatenate(bot_flattened,axis=0)
+    
+    # create training data
+    indices_top = np.arange(len(top_flattened))
+    indices_bot = np.arange(len(bot_flattened))
+
+    if randomize:
+        np.random.shuffle(indices_top)
+        np.random.shuffle(indices_bot)
+    
+    top_train_idx = int(len(top_flattened)*training)
+    bot_train_idx = int(len(bot_flattened)*training)
+
+    top_train = top_flattened[:top_train_idx,:]
+    bot_train = top_flattened[:bot_train_idx,:]
+
+    top_test = top_flattened[top_train_idx:,:]
+    bot_test = bot_flattened[bot_train_idx:,:]
+
+
+
+
+
+
         
     
     
