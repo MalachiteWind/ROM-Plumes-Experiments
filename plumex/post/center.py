b642827318e768715f89b248830a052ce74c45b7
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-10-02 13:39:56 -0700

    feat: add figure 6

--- plumex/post/center.py
+++ plumex/post/center.py
@@ -27,7 +27,17 @@ center_trial_keys = {
 }
 train_set = ("lo1", "865", "866", "867", "871", "913", "914", "917", "919", "hi1")
 test_set = ("868", "869", "916", "920", "hi2")
-low_se = ("lo1", "865", "866", "867", "868")
+low_set = ("lo1", "865", "866", "867", "868")
+med_set = ("914", "916", "917", "871")
+hi_set = ("hi1", "hi2", "919", "920")
+full_methods = {
+    "lin": "Linear",
+    "poly": "Polynomial",
+    "poly_para": "Parametric Quadratic",
+    "poly_inv_pin": "Square-Root",
+}
+
+
 trials_folder = Path(__file__).absolute().parents[2] / "trials"
 center_trials_dir = trials_folder / "center-regress"
 center_trial_data: dict[str, MultiRegressionResults] = {
@@ -46,13 +56,11 @@ def get_scores(res: MultiRegressionResults) -> dict[str, Float1D]:
         "lin": _get_non_nan_scores(res["regressions"]["linear"]),
         "poly": _get_non_nan_scores(res["regressions"]["poly"]),
         "poly_para": _get_non_nan_scores(res["regressions"]["poly_para"]),
-        "poly_inv": _get_non_nan_scores(res["regressions"]["poly_inv_pin"]),
+        "poly_inv_pin": _get_non_nan_scores(res["regressions"]["poly_inv_pin"]),
     }
 
 
 trial_scores = {vidkey: get_scores(res) for vidkey, res in center_trial_data.items()}
-train_vid_scores = {vidkey: trial_scores[vidkey] for vidkey in train_set}
-test_vid_scores = {vidkey: trial_scores[vidkey] for vidkey in test_set}
 
 
 def concat_video_scores(
@@ -60,17 +68,44 @@ def concat_video_scores(
 ) -> dict[str, Float1D]:
     return {
         meth: np.concatenate([scores[meth] for scores in vid_scores.values()], axis=0)
-        for meth in ("lin", "poly", "poly_para", "poly_inv")
+        for meth in ("poly_inv_pin", "lin", "poly_para", "poly")
     }
 
 
-train_scores = concat_video_scores(train_vid_scores)
-test_scores = concat_video_scores(test_vid_scores)
-low_scores = concat_video_scores()  # fill this in here
+train_scores = concat_video_scores(
+    {vidkey: trial_scores[vidkey] for vidkey in train_set}
+)
+test_scores = concat_video_scores({vidkey: trial_scores[vidkey] for vidkey in test_set})
+low_scores = concat_video_scores({vidkey: trial_scores[vidkey] for vidkey in low_set})
+med_scores = concat_video_scores({vidkey: trial_scores[vidkey] for vidkey in med_set})
+hi_scores = concat_video_scores({vidkey: trial_scores[vidkey] for vidkey in hi_set})
+all_min = min(min(accs) for accs in test_scores.values())
+all_max = max(max(accs) for accs in test_scores.values())
 
-[
-    plt.hist(scores, label=method)
-    for method, scores in train_scores.items()
-    if method != "poly"
-]
-plt.legend()
+
+# %%
+def compare_histogram(concat_scores):
+    plt.title("Plume-Center Regressions Performance on Test Set")
+    bins = np.linspace(0, 1.5, 10)
+    plt.hist(
+        [-score for score in concat_scores.values()],
+        label=[full_methods[method] for method in concat_scores.keys()],
+        bins=list(bins),
+    )
+    plt.ylabel("Number of frames")
+    plt.xlabel("Relative Extrapolation Error")
+    plt.legend()
+
+
+# %% Figure 6
+compare_histogram(test_scores)
+print(
+    r"""Caption: The square-root regression best extrapolated
+      the plume's drift away from the point source,
+      followed closely by linear regression (95% confidence level)."""
+)
+
+# %% Optional investigation figures - combined test/train
+compare_histogram(low_scores)
+compare_histogram(med_scores)
+compare_histogram(hi_scores)
