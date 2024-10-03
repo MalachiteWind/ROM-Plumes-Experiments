58b5191cc6ce02e026b3913d0ac4a3c40f05b1f5
Jake <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-10-02 17:31:22 -0700

    feat: Add runnable __init__ to plumex.post

---
+++ plumex/post/__init__.py
@@ -0,0 +1,12 @@
+"""Post analysis package.
+
+This contains a variety of scripts to generate final figures from
+trial data.  Modify the module constants for each script if new
+experiments are run.  Run individual scripts to generate respective
+figures/post analysis.  Run this module to generate all figures/post
+analysis.
+"""
+from . import center
+from . import points
+
+__all__ = ["center", "points"]
