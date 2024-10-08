268e7d6c2f049c9a8ac272b833f95569598b1b24
Ike Griss Salas <54322188+MalachiteWind@users.noreply.github.com>
2024-10-08 11:10:26 -0700

    cln: moved plotting to post subpackage

---
+++ plumex/post/__init__.py
@@ -0,0 +1,10 @@
+"""Post analysis package.
+
+This contains a variety of scripts to generate final figures from
+trial data.  Modify the module constants for each script if new
+experiments are run.  Run individual scripts to generate respective
+figures/post analysis.  Run this module to generate all figures/post
+analysis.
+"""
+from . import composite_plot
+
---
+++ plumex/post/composite_plot.py
@@ -0,0 +1,88 @@
+from typing import cast
+from typing import List
+
+import mitosis
+import numpy as np
+from ara_plumes.typing import PlumePoints
+
+from pathlib import Path
+
+from plumex.config import data_lookup
+from plumex.video_digest import _load_video
+from .post_utils import _visualize_fits
+from .post_utils import create_edge_func
+
+
+
+trials_folder = Path(__file__).absolute().parents[2] / "trials"
+
+# ignore 862 and 864
+# (center_regress_hash, video_lookup_word, edge_regress_hash)
+trail_lookup_key = {
+    "862": {
+        "default": ("85c44b", "low-862", "a52e31")
+    }
+}
+
+
+def _unpack_data(
+        center_regress_hash:str,
+        video_lookup_keyword: str,
+        edge_regress_hash: str,
+) -> dict:
+    
+    video, orig_center_fc = _load_video(data_lookup["filename"][video_lookup_keyword])
+    center_mitosis_step = mitosis.load_trial_data(
+        hexstr=center_regress_hash,
+        trials_folder=trials_folder / "center-regress"
+    )
+    edge_mitosis_step = mitosis.load_trial_data(
+        hexstr=edge_regress_hash,
+        trials_folder=trials_folder / "edge-regress"
+    )
+
+    center_fit_method = center_mitosis_step[1]["main"]
+    center_coeff_dc = center_mitosis_step[1]["regressions"][center_fit_method]["data"]
+
+    edge_plume_points = edge_mitosis_step[0]["data"]
+
+    center = cast(List[tuple[int, PlumePoints]], edge_plume_points["center"])
+    bot = cast(List[tuple[int, PlumePoints]], edge_plume_points["bottom"])
+    top = cast(List[tuple[int, PlumePoints]], edge_plume_points["top"]) 
+
+    top_edge_method, bot_edge_method = edge_mitosis_step[1]["main"]
+
+    edge_coefs_top = np.nanmean(
+        edge_mitosis_step[1]["accs"]["top"][top_edge_method]["coeffs"], axis=0
+    )
+    edge_coefs_bot = np.nanmean(
+        edge_mitosis_step[1]["accs"]["bot"][bot_edge_method]["coeffs"], axis=0
+    )
+
+    top_func = create_edge_func(edge_coefs_top, top_edge_method)
+    bot_func = create_edge_func(edge_coefs_bot, bot_edge_method)
+
+    start_frame = center[0][0]
+    end_frame = center[-1][0]
+
+    results = {
+        "video": video[start_frame: end_frame+1],
+        "center_coef": center_coeff_dc,
+        "center_func_method": center_fit_method,
+        "center_plume_points": center,
+        "top_plume_points": top,
+        "bottom_plume_points": bot,
+        "top_edge_func": top_func,
+        "bot_edge_func": bot_func,
+        "start_frame": start_frame,
+        "orig_center_fc": orig_center_fc,
+    }
+    return results
+
+
+
+# plot on true points
+_visualize_fits(n_frames=9, **_unpack_data(*trail_lookup_key["862"]["default"]))
+
+# plot on regression
+_visualize_fits(n_frames=9, **_unpack_data(*trail_lookup_key["862"]["default"]),plot_on_raw_points=False)
